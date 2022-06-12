63f2346a78c651fdd1f07d017dc68f9054fb1c8e
Patrick <patoread@gmail.com>
2022-06-12 22:03:41 +0200

    feat: Create homepage placeholder
    The homepage has been created. This will be de default page once the site is loaded. It still needs to be worked with but it now holds every element it's going to need

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="flex flex-col items-center justify-center">
+    <div>
+      <img
+        class="h-auto max-w-full"
+        src="@/assets/main-image.png"
+        alt="A girl sitting on a couch reading a book"
+      />
+    </div>
+    <div
+      class="flex flex-col items-center justify-center space-y-3 text-center"
+    >
+      <h1 class="text-3xl font-semibold">Share those moments</h1>
+      <p class="w-72 p-1 text-lg">
+        Unshelve the books you love and tell us your experience.
+      </p>
+      <button class="h- w-32 rounded bg-main-orange p-1 text-center text-white">
+        Start now
+      </button>
+    </div>
+  </main>
+</template>
