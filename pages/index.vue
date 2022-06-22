4df3d2abdfe88acd17dea9577ab7cbd466dd8006
Patrick <patoread@gmail.com>
2022-06-22 13:38:20 +0200

    chore: Rename test file
    The test file has been renamed to prevent typescript errors regarding the types of test

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
   >
     <div class="max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-2xl">
       <img
+        data-cy="main-image"
         class="h-auto max-w-full"
         src="@/assets/main-image.png"
         alt="A girl sitting on a couch reading a book"
@@ -12,15 +13,20 @@
     <div
       class="flex flex-col items-center justify-center space-y-2 text-center lg:space-y-5 lg:text-left"
     >
-      <h1 class="text-3xl font-medium md:text-4xl xl:text-5xl">
+      <h1
+        data-cy="main-title"
+        class="text-3xl font-medium md:text-4xl xl:text-5xl"
+      >
         Share those moments
       </h1>
       <p
+        data-cy="main-paragraph"
         class="w-72 p-1 text-lg lg:self-start lg:p-0 lg:text-xl xl:w-96 xl:text-2xl"
       >
         Unshelve the books you love and tell us your experience
       </p>
       <button
+        data-cy="main-button"
         class="w-32 bg-main-orange p-1 text-center text-white lg:self-start lg:p-2 lg:text-lg"
       >
         Start now
